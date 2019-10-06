0b1caf0479229c65000b0e121f3c4cdb86070383
afdagli <afdagli@gmail.com>
2019-10-06 12:58:39 +0300

    git debug example script

---
+++ unknown/debug_python.py
@@ -0,0 +1,26 @@
+
+
+import pdb
+
+def working_func(test, job, build):
+    print("Works good")
+    print("Works good...")
+    print("Works good.....")
+    print("Works good.......")
+    print("Works good.........")
+    print("Works good...........")
+    print("I guess something is wrong")
+    print("Yeah it is wrong, What's the wrong")
+    print("Yesss, I find the bug..")
+    print("Fixed ASAP")
+
+
+if __name__ == '__main__':
+    print('''
+c: continue execution
+w: shows the context of the current line it is executing.
+a: print the argument list of the current function
+s: Execute the current line and stop at the first possible occasion.
+n: Continue execution until the next line in the current function is reached or it returns.
+''')
+    working_func("main_test", "job_build_packge", "build2233")
